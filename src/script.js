e099ebb5741b1ec6b4ec73c09bf9e6a7083acf1c
Robbie Woolf <robbiemwoolf@gmail.com>
2022-07-02 13:31:44 -0700

    three cubes groups rotation scale position

--- src/script.js
+++ src/script.js
@@ -1,48 +1,69 @@
 import './style.css'
 import * as THREE from 'three'
 
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+/**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
 
-// Sizes
+/**
+ * Objects
+ */
+const group = new THREE.Group()
+group.position.y = .5;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube1.position.x = - 1.5;
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube2.position.x = 0;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube3.position.x = 1.5;
+group.add(cube3);
+
+/**
+ * Sizes
+ */
 const sizes = {
     width: 800,
     height: 600
 }
 
-// Camera
+/**
+ * Camera
+ */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+// camera.lookAt(new THREE.Vector3(0, - 1, 0))
 scene.add(camera)
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('canvas.webgl')
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
-
-
-// loading Three.js with a <script> has limitations
-// doesn't include some of the classes
-// we need those classes eventually
-// we need to run a server to emulate a website and for security reasons
-
-// What is a bundler?
-    // a tool in which you send JS, CSS, HTML, images, TypeScript, Stylus, Sass, etc
-    // The bundler apply potential modifications and output a web-friendly 'bundle'
-    // can do more like a local server, manage dependencies, improve compatibility, add modules support, optimize files, deploy, etc.
-
-// The bundler we are going to use is Webpack
-    // most popular
-    // handle most of our needs
-    // good documentation
-    // good community
-    // well maintained
-    // hard to configure
